b7aebad0e7c1ed71c358d24f8e5b0b4b56d8c487
Kristoffer Knape <kristoffer.knape@gmail.com>
2019-02-14 19:34:52 +0100

    Initial Commit

---
+++ Can I Borrow The CarTests/Can_I_Borrow_The_CarTests.swift
@@ -0,0 +1,34 @@
+//
+//  Can_I_Borrow_The_CarTests.swift
+//  Can I Borrow The CarTests
+//
+//  Created by Kristoffer Knape on 2019-02-14.
+//  Copyright © 2019 Kristoffer Knape. All rights reserved.
+//
+
+import XCTest
+@testable import Can_I_Borrow_The_Car
+
+class Can_I_Borrow_The_CarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
